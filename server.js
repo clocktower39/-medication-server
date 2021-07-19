35e7d57334f26fc61e686980494ede7343b2572a
clocktower39 <clocktower39@aol.com>
2021-07-19 13:55:12 -0700

    patient enrollment route

--- server.js
+++ server.js
@@ -32,19 +32,40 @@ var corsOptions = {
 }
 
 let Prescriber = mongoose.model('Prescriber', {
-    firstName: { type: String, required: true},
-    lastName: { type: String, required: true},
-    phoneNumber: { type: Number, required: true},
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
     faxNumber: { type: Number },
     npiNumber: { type: Number, required: true, index: { unique: true } },
     deaNumber: { type: String, required: true, index: { unique: true } },
-    practiceName: { type: String, required: true},
-    address1: { type: String, required: true},
+    practiceName: { type: String, required: true },
+    address1: { type: String, required: true },
     address2: { type: String },
-    city: { type: String, required: true},
-    state: { type: String, required: true},
-    zip: { type: Number, required: true},
-    country: { type: String, required: true},
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: Number, required: true },
+    country: { type: String, required: true} ,
+    patients: { 
+      active: { type: Array, required: true },
+      noLongerTreating: { type: Array, required: true }
+    }
+});
+
+let Patient = mongoose.model('Patient', {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
+    phoneNumber: { type: Number},
+    address1: { type: String, required: true },
+    address2: { type: String },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zip: { type: Number, required: true },
+    country: { type: String, required: true },
+    prescribers: { 
+      active: { type: Array, required: true },
+      removedFromCare: { type: Array, required: true }
+    }
 });
 
 app.post('/prescriberEnroll', (req, res) => {
@@ -65,6 +86,26 @@ app.post('/prescriberEnroll', (req, res) => {
       });
   }
   savePrescriber();
+})
+
+app.post('/patientEnroll', (req, res) => {
+  let patient = new Patient(req.body);
+  
+  let savePatient = () => {
+    patient.save((err)=>{
+          if(err){
+            console.log(err);
+            res.send({error: {err}});
+          }
+          else{
+              res.send({
+                  status: 'success',
+                  patient
+              })
+          }
+      });
+  }
+  savePatient();
 })
 
 mongoose.connect(dbUrl, 
